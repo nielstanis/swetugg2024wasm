512251eb6d9345dc5a66f7adbdf2b5cd6153daef
Niels Tanis <nielstanis@live.nl>
2024-09-16 13:08:15 +0200

    Slides

--- ConsoleAppWasi/Program.cs
+++ ConsoleAppWasi/Program.cs
@@ -2,3 +2,6 @@ using System;
 using System.Runtime.InteropServices;
 
 Console.WriteLine($"Hello Swetugg '24 Göteborg from {RuntimeInformation.OSArchitecture}!");
+
+var text = System.IO.File.ReadAllText("/etc/hosts");
+Console.WriteLine(text);
--- Extend/ConsoleApp/Program.cs
+++ Extend/ConsoleApp/Program.cs
@@ -4,17 +4,19 @@ Console.WriteLine("Time for SWETUGG '24 Göteborg!");
 Wasmtime.WasiConfiguration conf = new Wasmtime.WasiConfiguration()
     .WithInheritedStandardError()
     .WithInheritedStandardOutput()
+    .WithPreopenedDirectory("/etc","/etc")
+    .WithPreopenedDirectory(".",".")
     .WithArg("CalledFromConsole")
     .WithArgs(args); //passing args
 
 var engineConfig = new Wasmtime.Config();
-//engineConfig.WithFuelConsumption(true);
+engineConfig.WithFuelConsumption(true);
 var engine = new Wasmtime.Engine(engineConfig);
 var linker = new Wasmtime.Linker(engine);
 linker.DefineWasi();
 var store = new Wasmtime.Store(engine);
 store.SetWasiConfiguration(conf);
-//store.Fuel = 250;
+store.Fuel = 5000;
 
 string wasm = @"wasm/myrustmodule.wasm";
 var module = Wasmtime.Module.FromFile(engine, wasm);
@@ -24,4 +26,4 @@ var main = inst.GetFunction("main");
 if (main!=null)
     main.Invoke(0,0);
 
-//Console.WriteLine($"Consumed fuel: {store.Fuel}");
+Console.WriteLine($"Consumed fuel: {store.Fuel}");
